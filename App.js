b2f8106e6413244e4a3de7a292d2e7c3b5af2266
jozef <jozefchmelar@gmail.com>
2017-09-14 13:38:18 +0200

    new stuff

--- App.js
+++ App.js
@@ -11,7 +11,7 @@ import {
   Text,
   Image,
   View,
-  Button,
+  Button,FlatList,
   ToolbarAndroid,TouchableOpacity
 } from 'react-native';
 import { StackNavigator } from 'react-navigation';
@@ -30,8 +30,7 @@ class MainScreen extends Component {
         flex: 1,
         flexDirection: 'column',
         justifyContent: 'center'
-      }}>
-     
+      }}>   
       
         <View style={[styles.container, styles.card]} >
 
@@ -40,7 +39,7 @@ class MainScreen extends Component {
         </TouchableOpacity>
           
           <Text style={styles.bigText}>Standart match</Text>
-          <Text style={styles.smallerText}>Free match 2 on 2</Text>
+          <Text style={[styles.smallerText,{marginBottom:16}]}>Free match 2 on 2</Text>
           
         </View>
 
@@ -56,10 +55,42 @@ class MainScreen extends Component {
 }
 
 class GameScreen extends Component {
+     
     static navigationOptions = {
       title: 'Free match',
       backgroundColor: '#3c97f4'
     };
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          loading: false,
+          data: [{key:1,winner:'blue'},{key:21,winner:'red'},{key:31,winner:'blue'},{key:41,winner:'blue'},{key:18,winner:'red'}],
+          scoreRed  : 0 ,
+          scoreBlue : 0,
+          error: null,
+          refreshing: false,
+        };
+      }
+    
+    _keyExtractor = (item, index) => item.key;
+    
+    _renderGoal = ({item}) => (
+        <View style={[ styles.card]} >
+            <View style={{flex:100,flexDirection:'row'}}>
+                <View style={{flex:20}}>
+                    {item.winner=='red' ? <Image style={{margin:16,width:50, height:50}} source={require('./res/pic/cervenychlap.png')}/> : <Image  style={{margin:16,width:50, height:50}} source={require('./res/pic/modrychlap.png')}/> }
+                </View>
+                <View style={{flex:80,margin:16,flexDirection:'column',justifyContent:'center'}}>
+                    {item.winner=='red' ?
+                        <Text style={[styles.mediumText,{color:'#ea5859'}]} >CERVENY GOL</Text> :
+                    <Text style={[styles.mediumText,{color:'#4b90e2'}]}>MODRY GOoL</Text>}
+                </View>
+            </View>
+        </View>
+  );
+
   render() {
     const { navigate } = this.props.navigation;   
     return (        
@@ -67,29 +98,49 @@ class GameScreen extends Component {
         <View style={{flex :30 ,backgroundColor:'white',flexDirection:'row' ,justifyContent:'space-between'}}>
          
             <View style={{flexDirection:'column',margin:16,justifyContent:'center',alignItems:'center'}}>
+            <TouchableOpacity  onPress={() => this.state.data.push({key:57987,winner:'red'})}>
                 <Image  style={{width:100 ,height:100}} source={require('./res/pic/cervenychlap.png')}/>
-                <Text>RED TEAM</Text>        
+                <Text>RED TEAM</Text>      
+                </TouchableOpacity>  
             </View>
 
 
             <View style={{flexDirection:'column',margin:16,justifyContent:'center',alignItems:'center'}}>
-                <Text style={styles.bigText}>0:0</Text>      
+                <View style={{flexDirection:'row'}}>
+                    <Text style={[styles.bigText,{color:'#ea5859'}]}>{this.state.scoreRed}</Text>
+                    <Text style={[styles.bigText,{color:'black'}]}>:</Text>
+                    <Text style={[styles.bigText,{color:'#4b90e2'}]}>{this.state.scoreBlue}</Text>
+                    
+                </View>
                 <Text>0:0,0:0,0:0</Text>      
             </View>
             
             <View style={{flexDirection:'column',margin:16,justifyContent:'center',alignItems:'center'}}>
+                <TouchableOpacity  onPress={() => this.state.data.push({key:555,winner:'blue'})}>
                 <Image style={{width:100 ,height:100}}  source={require('./res/pic/modrychlap.png')}/>
-                <Text>Blue TEAM</Text>        
+                <Text>BLUE TEAM</Text>        
+                </TouchableOpacity>
             </View>
 
         </View>
-        <View style={{flex :70,backgroundColor:'#f1f2f5' }}></View>
+        <View style={{flex :70,backgroundColor:'#f1f2f5' }}>
+        <FlatList
+        keyExtractor={this._keyExtractor}
+        
+                   data={this.state.data}
+           
+            renderItem={this._renderGoal}
+        />
+        </View>
      </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+    mediumText:{
+        fontSize: 14    
+    },
   bigText: {
     color: '#4a9eff',
     fontSize: 28
